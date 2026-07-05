48555e8a56c365409476dc8d2564757e551108bc
Panth Patel <panth2711200@gmail.com>
2026-07-06 03:22:33 +0530

    fix login API route and axios setup

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 const base_url = "https://hire-hub-backend-2.onrender.com/"
 
 const loginUser = async (login:any)=> {
-    return axios.post(`${base_url}login`, login)
+    return axios.post(`${base_url}/auth/login`, login)
     .then(result => result.data)
     .catch(error =>{throw error; });
 }
